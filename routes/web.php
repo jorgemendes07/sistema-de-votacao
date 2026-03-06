18954471aec4622f34b92418530fc5a333484003
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-03-06 17:43:11 -0300

    fix: remove funcoes e rota de PollOptionController

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@ Route::get('/', [PollController::class, 'index'])->name('home');
 // Enquetes
 Route::resource('polls', PollController::class);
 
-// Opções de enquete 
-Route::resource('polls.options', PollOptionController::class);
-
 // Votar
 Route::get('polls/{poll}/vote', [PollController::class, 'show'])->name('polls.vote');
 Route::post('polls/{poll}/vote', [PollController::class, 'vote'])->name('polls.vote.submit');
