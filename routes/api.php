6e34eb8291822e6e94ad81feaa9fcec3fe92724c
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-07-29 22:07:10 -0300

    feat: registra rotas de autenticacao e enquetes na API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PollController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+});
+
+Route::get('/polls', [PollController::class, 'index']);
+Route::get('/polls/{poll}', [PollController::class, 'show']);
+Route::post('/polls/{poll}/vote', [PollController::class, 'vote']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/polls', [PollController::class, 'store']);
+    Route::put('/polls/{poll}', [PollController::class, 'update']);
+    Route::patch('/polls/{poll}', [PollController::class, 'update']);
+    Route::delete('/polls/{poll}', [PollController::class, 'destroy']);
+});
