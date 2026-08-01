24219285f2359cb568c66377ee49aeb591e5d949
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-07-31 23:51:34 -0300

    refactor: substitui views blade por respostas JSON

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PollController;
-use App\Http\Controllers\PollOptionController;
 
-// Página inicial
-Route::get('/', [PollController::class, 'index'])->name('home');
-
-// Enquetes
-Route::resource('polls', PollController::class);
-
-// Votar
-Route::get('polls/{poll}/vote', [PollController::class, 'show'])->name('polls.vote');
-Route::post('polls/{poll}/vote', [PollController::class, 'vote'])->name('polls.vote.submit');
+Route::get('/', function () {
+    return response()->json(['message' => 'Sistema de Votação API']);
+})->name('home');
