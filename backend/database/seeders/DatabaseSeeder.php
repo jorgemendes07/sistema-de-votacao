c5bb0915b135d12ae4db4a3412a44e7fa614c2bc
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-08-05 13:20:38 -0300

    feat: altera dados do seeder com enquetes iniciais

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,30 +12,30 @@ class DatabaseSeeder extends Seeder
     {
         // Enquete 1
         $poll1 = Poll::create([
-            'title' => 'Qual o melhor modelo de estudo?',
-            'start_date' => now()->subDays(3),
+            'title' => 'Qual modalidade de estudo você prefere?',
+            'start_date' => now()->addDays(3),
             'end_date' => now()->addDays(3),
         ]);
 
-        PollOption::create(['poll_id' => $poll1->id, 'option_text' => 'Presencial', 'votes' => 4]);
-        PollOption::create(['poll_id' => $poll1->id, 'option_text' => 'Híbrido', 'votes' => 1]);
-        PollOption::create(['poll_id' => $poll1->id, 'option_text' => 'Educação à distância', 'votes' => 3]);
+        PollOption::create(['poll_id' => $poll1->id, 'option_text' => 'Presencial', 'votes' => 0]);
+        PollOption::create(['poll_id' => $poll1->id, 'option_text' => 'Híbrido', 'votes' => 0]);
+        PollOption::create(['poll_id' => $poll1->id, 'option_text' => 'Educação à distância', 'votes' => 0]);
 
         // Enquete 2
         $poll2 = Poll::create([
-            'title' => 'Qual banda não pode faltar no Rock in Rio?',
+            'title' => 'Você utiliza ferramentas de IA no seu dia a dia de desenvolvimento?',
             'start_date' => now()->subDays(10),
             'end_date' => now()->subDays(2),
         ]);
 
-        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Metallica', 'votes' => 12]);
-        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Iron Maiden', 'votes' => 8]);
-        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Megadeth', 'votes' => 3]);
-        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'AC/DC', 'votes' => 5]);
+        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Diariamente', 'votes' => 18]);
+        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Algumas vezes por semana', 'votes' => 7]);
+        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Raramente', 'votes' => 4]);
+        PollOption::create(['poll_id' => $poll2->id, 'option_text' => 'Nunca', 'votes' => 1]);
 
         // Enquete 3
         $poll3 = Poll::create([
-            'title' => 'Qual linguagem de programação você utiliza para back end atualmente?',
+            'title' => 'Qual linguagem de programação você mais utiliza para back-end?',
             'start_date' => now(),
             'end_date' => now()->addDays(4),
         ]);
@@ -45,17 +45,6 @@ class DatabaseSeeder extends Seeder
         PollOption::create(['poll_id' => $poll3->id, 'option_text' => 'JavaScript', 'votes' => 5]);
         PollOption::create(['poll_id' => $poll3->id, 'option_text' => 'C#', 'votes' => 3]);
         PollOption::create(['poll_id' => $poll3->id, 'option_text' => 'Java', 'votes' => 2]);
-
-        // Enquete 4
-        $poll4 = Poll::create([
-            'title' => 'Qual o melhor pet?',
-            'start_date' => now()->addDays(1),
-            'end_date' => now()->addDays(10),
-        ]);
-
-        PollOption::create(['poll_id' => $poll4->id, 'option_text' => 'Gato']);
-        PollOption::create(['poll_id' => $poll4->id, 'option_text' => 'Cachorro']);
-        PollOption::create(['poll_id' => $poll4->id, 'option_text' => 'Peixe']);
-        PollOption::create(['poll_id' => $poll4->id, 'option_text' => 'Pássaro']);
+        PollOption::create(['poll_id' => $poll3->id, 'option_text' => 'Outra', 'votes' => 2]);
     }
 }
