93aec4b42fd36d83942c997d77b732f3011d7c1a
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-08-04 23:17:16 -0300

    feat: adiciona pill de usuario com opcao de logout no header

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, HostListener, computed, inject, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
 import { AuthService } from '../../core/services/auth';
@@ -11,16 +11,33 @@ import { AuthService } from '../../core/services/auth';
 export class Header {
   protected readonly auth = inject(AuthService);
   private readonly router = inject(Router);
+  private readonly elementRef = inject(ElementRef<HTMLElement>);
 
   protected readonly isHome = signal(this.router.url === '/');
+  protected readonly firstName = computed(() => this.auth.user()?.name.split(' ')[0] ?? '');
+  protected readonly initial = computed(() => this.firstName().charAt(0).toUpperCase());
+  protected readonly isMenuOpen = signal(false);
 
   constructor() {
     this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
       this.isHome.set(event.urlAfterRedirects === '/');
+      this.isMenuOpen.set(false);
     });
   }
 
+  @HostListener('document:click', ['$event'])
+  protected onDocumentClick(event: MouseEvent): void {
+    if (this.isMenuOpen() && !this.elementRef.nativeElement.contains(event.target as Node)) {
+      this.isMenuOpen.set(false);
+    }
+  }
+
+  protected toggleMenu(): void {
+    this.isMenuOpen.update((open) => !open);
+  }
+
   protected logout(): void {
+    this.isMenuOpen.set(false);
     this.auth.logout().subscribe(() => this.router.navigate(['/']));
   }
 }
